aacbda4544644596e847b72224f6e9f35b6d8794
olesgedz <gedzoles@gmail.com>
2020-01-07 06:44:14 +0300

    jump basic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    [SerializeField] Rigidbody2D player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        player.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), player.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+             player.velocity = new Vector2(player.velocity.x, jumpForce);
+        }
+    }
+}
