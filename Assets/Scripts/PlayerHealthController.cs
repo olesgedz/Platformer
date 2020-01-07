e1752da821f9604ec134a8f5861c86d563372161
olesgedz <gedzoles@gmail.com>
2020-01-08 00:32:15 +0300

    Singleton game controller

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+    [SerializeField] int maxHealth, currentHealth;
+
+    private void Awake()
+    {
+        instance = this;    
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;    
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        currentHealth--;
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
