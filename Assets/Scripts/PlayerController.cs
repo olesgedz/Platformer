eb37333144448631934606f7396a40ea2c9c7d5d
olesgedz <gedzoles@gmail.com>
2020-01-07 07:14:30 +0300

    double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-
     [SerializeField] Rigidbody2D player;
+    private bool isGrounded;
+    private bool isAllowedToDoubleJump;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] LayerMask whatIsGround;
+
     void Start()
     {
         
@@ -16,9 +20,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         player.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), player.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        Debug.Log(isGrounded);
+        if (isGrounded)
+            isAllowedToDoubleJump = true;
         if (Input.GetButtonDown("Jump"))
         {
-             player.velocity = new Vector2(player.velocity.x, jumpForce);
+            if (isGrounded)
+                player.velocity = new Vector2(player.velocity.x, jumpForce);
+            else if (isAllowedToDoubleJump)
+            {
+                player.velocity = new Vector2(player.velocity.x, jumpForce);
+                isAllowedToDoubleJump = false;
+            }
         }
     }
 }
